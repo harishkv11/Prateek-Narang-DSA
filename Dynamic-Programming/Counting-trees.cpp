995ef098ed852ec9f8373eb74eadb441481faf5b
Harish <iamharish.maniax@gmail.com>
2021-09-23 01:14:51 +0530

    Added File

---
+++ Dynamic-Programming/Counting-trees.cpp
@@ -0,0 +1,109 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define forn(i,n)       for(int i=0;i<n;i++)
+#define ff              first
+#define ss              second
+#define int             long long
+#define pb              push_back
+#define mp              make_pair
+#define pii             pair<int,int>
+#define vi              vector<int>
+#define mii             map<int,int>
+#define pqb             priority_queue<int>
+#define pqs             priority_queue<int,vi,greater<int> >
+#define setbits(x)      __builtin_popcountll(x)
+#define zrobits(x)      __builtin_ctzll(x)
+#define leadingzr(x)    __builtin_clz(x)
+#define mod             1000000007
+#define inf             1e18
+#define ps(x,y)         fixed<<setprecision(y)<<x
+#define mk(arr,n,type)  type *arr=new type[n];
+#define arr(a,n)          int n;cin>>n;vector<int> a(n);for(int i=0;i<n;i++) cin>>a[i];
+#define w(x)            int x; cin>>x; while(x--)
+#define conarr(a,n,k)     int n,k;cin>>n>>k;vector<int> a(n);for(int i=0;i<n;i++) cin>>a[i];
+#define range(x)           (x).begin(),(x).end()
+#define endl            "\n"
+#define total(x)        accumulate(x.begin(),x.end(),(int)0)
+ 
+int add(int x,int y) {int res=x+y; return (res>=mod ? res-mod: res);}
+int mul(int x,int y) {int res=x*y; return (res>=mod? res%mod : res);}
+int sub(int x,int y) {int res=x-y; return (res<0?res+mod:res);}
+int power(int x,int y) {int res=1; x%=mod; while(y){ if(y&1) res=mul(res,x); y>>=1; x=mul(x,x);} return res;}
+int mod_inv(int x) {return power(x,mod-2);}
+
+void hkv()
+{
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#ifndef ONLINE_JUDGE
+    //freopen("input.txt", "r", stdin);
+    //freopen("output.txt", "w", stdout);
+    freopen("error.txt","w", stderr);
+#endif
+}
+
+//-----------debugging-------------
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x <<" "; _print(x); cerr << endl;
+#else
+#define debug(x)
+#endif
+
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(long double t) {cerr << t;}
+void _print(double t) {cerr << t;}
+void _print(unsigned long long t) {cerr << t;}
+
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.ff); cerr << ","; _print(p.ss); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(unordered_set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(unordered_map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+
+
+//---------------------------------
+
+//O(n^2)
+int catalan(int n){
+    vector<int> dp(n+1);
+    dp[0]=dp[1]=1;
+    for(int i=2;i<=n;i++){
+        dp[i]=0;
+        for(int j=0;j<i;j++)
+            dp[i]+=(dp[j]*dp[i-j-1]);
+    }
+    return dp[n];
+}
+
+//O(n)
+int catalan_optim(int n){
+    vector<int> dp(n+1);
+    dp[0]=1;
+    for(int i=1;i<=n;i++){
+        dp[i]=((2*i)*(2*i-1)*dp[i-1])/(i*(i+1));
+    }
+    return dp[n];
+}
+
+void solve(){
+    int n;cin>>n;
+    cout<<catalan(n)<<endl;
+    cout<<catalan_optim(n)<<endl;
+}
+
+int32_t main()
+{
+    hkv();
+    //solve();
+    w(t) solve();
+    return 0;
+}
