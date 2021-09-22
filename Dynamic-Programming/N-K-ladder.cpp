64ca93b5b24de4f7d83a9a01b71f66a5c558b255
Harish <iamharish.maniax@gmail.com>
2021-09-23 00:20:00 +0530

    Modified File

--- Dynamic-Programming/N-K-ladder.cpp
+++ Dynamic-Programming/N-K-ladder.cpp
@@ -95,11 +95,20 @@ int solveiter(int n,int k){
     return dp[n];
 }
 
+//Optimised 
+int solveoptim(int n,int k){
+    vector<int> dp(n+1);
+    dp[0]=dp[1]=1;
+    for(int i=2;i<=k;i++) dp[i]=2*dp[i-1];
+    for(int i=k+1;i<=n;i++) dp[i]=2*dp[i-1]-dp[i-k-1];
+    return dp[n]; 
+}
 void solve(){
     int n,k;cin>>n>>k;
     vector<int> dp(n+1,-1);
     cout<<solverec(n,k,dp)<<endl;   
     cout<<solveiter(n,k)<<endl;
+    cout<<solveoptim(n,k)<<endl;
 }
 
 int32_t main()
